e1078c004443c6a6930544d1881ed25be519431c
Morgan Creekmore <gamingrobot@gmail.com>
2014-08-03 03:26:10 -0600

    Added clear and reset

--- server/game_server/main.go
+++ server/game_server/main.go
@@ -199,6 +199,7 @@ func main() {
 					}
 					websockets.consoleToId[currentVm] = append(websockets.consoleToId[currentVm], websocketId)
 					websockets.Unlock()
+					ws.WriteMessage(websocket.TextMessage, []byte(CLEAR))
 					ws.WriteMessage(websocket.TextMessage, []byte(RESET))
 					//ws.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("Selected Container %d\r\n", currentVm)))
 					ws.WriteMessage(websocket.TextMessage, []byte(consoleBuffers[currentVm]))
