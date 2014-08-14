951d35fa8eb2f6aa7ce5dba9029740407b2d7e50
Morgan Creekmore <gamingrobot@gmail.com>
2014-08-13 19:48:26 -0600

    Fixed build script to not set gvm

--- ops/build.sh
+++ ops/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 echo "I am the build script that is ran on ginux-build."
-gvm install go1.2
-declare -x PATH="/root/.gvm/pkgsets/go1.2/global/bin:/root/.gvm/gos/go1.2/bin:/root/.gvm/pkgsets/go1.2/global/overlay/bin:/root/.gvm/bin:/root/.gvm/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin"
-declare -x GOPATH="/root/.gvm/pkgsets/go1.2/global"
-declare -x GOROOT="/root/.gvm/gos/go1.2"
 cd server/game_server
 go get
 go build
