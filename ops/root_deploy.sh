e204f2bb01fc1f7291adf8c2f61999ec78bdaa99
Morgan Creekmore <gamingrobot@gmail.com>
2014-08-13 20:05:44 -0600

    Fixed paths on deploy scripts

--- ops/root_deploy.sh
+++ ops/root_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "I am the deploy script that is ran on ginux-dev as root"
 killall vzcontrol
-cd /home/ginux/ginux/
+cd /home/ginux/
 cd server/vzcontrol
 screen -dmSL VZC ./vzcontrol
